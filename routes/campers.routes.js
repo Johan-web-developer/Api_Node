f3b21111eb41d86b4df32a4441483af44e0e563f
Johan Florez <131258981+Johan-web-developer@users.noreply.github.com>
2023-09-25 15:57:16 -0500

    feat: :sparkles: Crud Campers listo

--- routes/campers.routes.js
+++ routes/campers.routes.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 const express = require('express');
 const router = express.Router();
 const bases = process.env.DBBD;
@@ -44,7 +44,7 @@ router.delete('/eliminar_camper/:id', async (req, res) => {
         const camperId = req.params.id;
       
         try {
-          const result = await collection.deleteOne({ _id: ObjectId(camperId) });
+          const result = await collection.deleteOne({ _id: new ObjectId(camperId) });
           if (result.deletedCount === 1) {
             res.json({ message: 'Camper eliminado con éxito' });
           } else {
@@ -55,4 +55,32 @@ router.delete('/eliminar_camper/:id', async (req, res) => {
         }
       });     
 
+      router.put('/actualizar_camper/:id', async (req, res) => {
+        const client = new MongoClient(bases, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        const db = client.db('Artemis');
+        const collection = db.collection('campers');
+        const camperId = req.params.id;
+      
+        const { nombre, edad, promedio, nivelCampus, nivelIngles, especialidad, expertoTecnologia, detalle, direccion, celular } = req.body;
+      
+        const updateCamper = {
+          $set: {
+            nombre, edad, promedio, nivelCampus, nivelIngles, especialidad, expertoTecnologia, detalle, direccion, celular
+          }
+        };
+      
+        try {
+          const result = await collection.updateOne({ _id: new ObjectId(camperId) }, updateCamper);
+      
+          if (result.modifiedCount === 1) {
+            res.json({ message: 'Camper actualizado con éxito' });
+          } else {
+            res.status(404).json({ message: 'No se encontró el camper con el ID proporcionado' });
+          }
+        } catch (error) {
+          res.status(500).json({ message: 'Error al actualizar el camper', error: error.message });
+        }
+      });
+      
 module.exports = router;
