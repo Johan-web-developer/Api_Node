57ddf27cf9aa822213aceee9e7ca4744c3e88dd3
Johan-web-developer <johan.flore.13@gmail.com>
2023-10-02 13:16:05 -0500

    feat: :sparkles: listo el fetch de mi API

--- react/fetch_api/src/App.js
+++ react/fetch_api/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './App.css'
 
 const TuComponente = () => {
   const [campers, setCampers] = useState([]);
@@ -6,7 +7,7 @@ const TuComponente = () => {
   useEffect(() => {
     const obtenerCampers = async () => {
       try {
-        const response = await fetch('http://localhost:4000/api/todos_los_campers'); // Asegúrate de que la URL sea correcta
+        const response = await fetch('http://localhost:4000/api/todos_los_campers');
         const data = await response.json();
         setCampers(data);
       } catch (error) {
@@ -20,12 +21,42 @@ const TuComponente = () => {
   return (
     <div>
       <h1>Campers</h1>
-      <ul>
-        {campers.map(camper => (
-          <li key={camper._id}>{camper.nombre}</li>
-          // Asegúrate de cambiar "nombre" por el campo correcto en tu base de datos
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Imagen</th>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>Promedio</th>
+            <th>Nivel-Campus</th>
+            <th>Nivel-Ingles</th>
+            <th>Especialidad</th>
+            <th>Experto-Tecnologia</th>
+            <th>Detalle</th>
+            <th>Dirección</th>
+            <th>Celular</th>
+          </tr>
+        </thead>
+        <tbody>
+          {campers.map(camper => (
+            <tr key={camper._id}>
+              <td><img src={`./assets/${camper.imagen}`}></img></td>
+              <td>{camper.id}</td>
+              <td>{camper.nombre}</td>
+              <td>{camper.edad}</td>
+              <td>{camper.promedio}</td>
+              <td>{camper.nivelCAmpus}</td>
+              <td>{camper.nivelIngles}</td>
+              <td>{camper.especialidad}</td>
+              <td>{camper.expertoTecnologia}</td>
+              <td>{camper.detalle}</td>
+              <td>{camper.direccion}</td>
+              <td>{camper.celular}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
