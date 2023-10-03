989dcf05a6f74504ed528b27a1425fe375fc1ad1
Johan-web-developer <johan.flore.13@gmail.com>
2023-10-03 17:56:59 -0500

    feat: :sparkles: avance en el front

--- frontend/fetch_api/src/App.js
+++ frontend/fetch_api/src/App.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from '@chakra-ui/react'
 import './App.css'
 
 const TuComponente = () => {
@@ -21,51 +30,60 @@ const TuComponente = () => {
 
   return (
     <div>
-      <h1>Campers</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Imagen</th>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Edad</th>
-            <th>Promedio</th>
-            <th>Nivel-Campus</th>
-            <th>Nivel-Ingles</th>
-            <th>Especialidad</th>
-            <th>Experto-Tecnologia</th>
-            <th>Detalle</th>
-            <th>Dirección</th>
-            <th>Celular</th>
-            <th>Actualizar Camper</th>
-            <th>Eliminar Camper</th>
-          </tr>
-        </thead>
-        <tbody>
-          {campers.map(camper => (
-            <tr key={camper._id}>
-              <td><img src={`/assets/${camper.imagen}`}></img></td>
-              <td>{camper.id}</td>
-              <td>{camper.nombre}</td>
-              <td>{camper.edad}</td>
-              <td>{camper.promedio}</td>
-              <td>{camper.nivelCAmpus}</td>
-              <td>{camper.nivelIngles}</td>
-              <td>{camper.especialidad}</td>
-              <td>{camper.expertoTecnologia}</td>
-              <td>{camper.detalle}</td>
-              <td>{camper.direccion}</td>
-              <td>{camper.celular}</td>
-              <td>
-                 <Button variant="contained" color="primary">Actualizar</Button>
-              </td>
-              <td>
-                <Button variant="contained" color="warning">Eliminar</Button>
-                </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Campers</a>
+        </li>
+        <li className="navbar-item">
+          <a href="#" className="navbar-link">Trainers</a>
+        </li>
+      </ul>
+    </nav>
+      <TableContainer>
+  <Table size='sm'>
+    <Thead>
+      <Tr>
+        <Th isNumeric>ID</Th>
+        <Th>Nombre</Th>
+        <Th>Edad</Th>
+        <Th>Promedio</Th>
+        <Th>Nivel-Campus</Th>
+        <Th>Nivel-Ingles</Th>
+        <Th>Especialidad</Th>
+        <Th>Experto-Tecnologia</Th>
+        <Th>Detalle</Th>
+        <Th>Dirección</Th>
+        <Th>Celular</Th>
+        <Th>Actualizar Camper</Th>
+        <Th>Eliminar Camper</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      {campers.map(camper => (
+        <Tr key={camper._id}>
+          <Td>{camper.id}</Td>
+          <Td>{camper.nombre}</Td>
+          <Td>{camper.edad}</Td>
+          <Td>{camper.promedio}</Td>
+          <Td>{camper.nivelCAmpus}</Td>
+          <Td>{camper.nivelIngles}</Td>
+          <Td>{camper.especialidad}</Td>
+          <Td>{camper.expertoTecnologia}</Td>
+          <Td>{camper.detalle}</Td>
+          <Td>{camper.direccion}</Td>
+          <Td>{camper.celular}</Td>
+          <Td>
+            <Button variant="contained" color="primary">Actualizar</Button>
+          </Td>
+          <Td>
+            <Button variant="contained" color="warning">Eliminar</Button>
+          </Td>
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+</TableContainer>
     </div>
   );
 };
